c4108de65e017fd290aa823b5065acd788a4d9e8
safe-web <13763350076@163.com>
2025-03-10 16:06:23 +0800

    Update app6.py

--- app6.py
+++ app6.py
@@ -105,7 +105,7 @@ elif mode == "plot":
     
     if st.button("Plot"):
         try:
-           Graph_Plotting()
+            Graph_Plotting()
             if "mapping_history" not in st.session_state:
                 st.session_state.mapping_history = []
             st.session_state.mapping_history.append(expression)      
